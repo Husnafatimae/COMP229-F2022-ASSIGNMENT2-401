b74574e606d89d99c1c393f5307636737ff47dcb
Husna Fatima <hfatima4@my.centennialcollege.ca>
2022-10-23 19:31:14 -0400

    added authentication

--- app/routes/bcontacts.route.server.js
+++ app/routes/bcontacts.route.server.js
@@ -6,14 +6,16 @@ import {  DisplayBcontactsList,
     ProcessBcontactsUpdatePage, 
     DisplayBcontactsUpdatePage, 
     ProcessBcontactsDelete } from "../controllers/bcontacts.controller.server.js";
+   
+    import { AuthGuard } from "../utils/index.js";
 
 const router = Router();
 
 router.get('/bcontact-list', DisplayBcontactsList);
-router.get('/bcontact-add', DisplayBcontactsAddPage);
-router.post('/bcontact-add', ProcessBcontactsAddPage);
-router.post('/bcontact-update/:id', ProcessBcontactsUpdatePage);
-router.get('/bcontact-update/:id', DisplayBcontactsUpdatePage);
-router.get('/bcontact-delete/:id', ProcessBcontactsDelete);
+router.get('/bcontact-add', AuthGuard, DisplayBcontactsAddPage);
+router.post('/bcontact-add', AuthGuard, ProcessBcontactsAddPage);
+router.post('/bcontact-update/:id', AuthGuard, ProcessBcontactsUpdatePage);
+router.get('/bcontact-update/:id', AuthGuard, DisplayBcontactsUpdatePage);
+router.get('/bcontact-delete/:id', AuthGuard, ProcessBcontactsDelete);
 
 export default router;
